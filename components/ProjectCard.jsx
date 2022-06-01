8afc35269e46e0bd9f0fc658aab6978f5c6b4765
bettyspurgeon <betty.spurgeon713@gmail.com>
2022-06-01 16:21:20 +0200

    fix className

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -8,21 +8,21 @@ export const ProjectCard = ({
 }) => {
   return (
     <>
-      <div class="card mb-3" style={{ width: 32 + "%" }}>
-        <div class="card-body">
-          <h5 class="card-title fs-3 align-middle">{projectTitle}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">
+      <div className="card mb-3" style={{ width: 32 + "%" }}>
+        <div className="card-body">
+          <h5 className="card-title fs-3 align-middle">{projectTitle}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">
             Technologies Used: {projectTech}
           </h6>
-          <p class="card-text">{projectSummary}</p>
-          <a href={githubLink} target="_blank" class="card-link">
+          <p className="card-text">{projectSummary}</p>
+          <a href={githubLink} target="_blank" className="card-link">
             {iconLinks[0].icon}
           </a>
           <a
             className="text-decoration-none"
             href={liveLink}
             target="_blank"
-            class="card-link px-2"
+            className="card-link px-2"
           >
             Live
           </a>
